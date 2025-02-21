7f8ebf0f60b59e5eec283f22b287d8bb0d54ecf9
Juan Rosas <juan.rosasr01@gmail.com>
2025-02-21 08:15:52 -0400

    add scripts to backup

--- backup_docker_postgres.sh
+++ backup_docker_postgres.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Configura estas variables
-CONTAINER_NAME=""
+CONTAINER_NAME="lunatv-postgres"
 BACKUP_DIR_CONTAINER="/home"  # Ajusta según la ubicación de tu base de datos en el contenedor
-BACKUP_DIR_LOCAL="/home/jrosasr/backups/"
+BACKUP_DIR_LOCAL="/home/admin/backups/"
 USER=""
 DATABASE=""
 
@@ -11,7 +11,7 @@ DATABASE=""
 docker exec -it "$CONTAINER_NAME" mkdir -p "$BACKUP_DIR_CONTAINER"
 
 # Generar el nombre del archivo de respaldo
-BACKUP_FILE="backup_postgres16_$(date +%Y_%m_%d_%H%M%S).sql"
+BACKUP_FILE="backup_postgres17_$(date +%Y_%m_%d_%H%M%S).sql"
 
 # Ejecutar pg_dump dentro del contenedor y guardar el archivo en el volumen
 docker exec -it "$CONTAINER_NAME" pg_dump -U "$USER" -d "$DATABASE" -f "$BACKUP_DIR_CONTAINER/$BACKUP_FILE"
--- backup_server_postgres.sh
+++ backup_server_postgres.sh
@@ -6,7 +6,7 @@ USER=""
 DATABASE=""
 
 # Generar el nombre del archivo de respaldo
-BACKUP_FILE="backup_postgres14_$(date +%Y_%m_%d_%H%M%S).sql"
+BACKUP_FILE="backup_postgres17_$(date +%Y_%m_%d_%H%M%S).sql"
 
 # Ejecutar pg_dump directamente en el servidor y guardar el archivo
 pg_dump -U "$USER" -d "$DATABASE" -f "$BACKUP_DIR_LOCAL/$BACKUP_FILE"
